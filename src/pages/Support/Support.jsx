f55b94a7f1a3afccce33aea93e9dfbc8f7f84a6a
mayankr31 <mayankaryabhatt.1@gmail.com>
2024-12-03 00:38:21 +0530

    Dashboard page completed

---
+++ src/pages/Support/Support.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Text} from '@chakra-ui/react'
+import DashboardLayout from '../../components/DashboardLayout'
+
+export default function Support(props) {
+  
+
+  return (
+      <DashboardLayout title="Support">
+
+      </DashboardLayout>
+    
+  )
+}
---
+++ src/pages/Transaction/Transaction.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardLayout from '../../components/DashboardLayout'
+
+export default function Transaction(props) {
+  
+
+  return (
+    <DashboardLayout title="Transactions">
+
+    </DashboardLayout>
+  )
+}
